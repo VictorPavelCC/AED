a5bff5dcf020d98fa0488bc9aa289c2328ee337e
Victor Pavel Cuentas Calle <pavelcuentas@gmail.com>
2023-05-02 20:21:42 -0300

    Actualizando y Organizando repo

--- 00_HelloWorld/README.md
+++ 00_HelloWorld/README.md
@@ -1,5 +1,5 @@
-## Algoritmos y estructura de Datos
 ## Curso: K1051 2023
 ## Legajo: 1771073
 ## Nombre: Victor Pavel 
 ## Apellido:Cuentas Calle
+## Trabajo N°0
---
+++ 00_HelloWorld/hola.cpp
@@ -0,0 +1,15 @@
+/*Trabajo 0
+  Descripcion: Hola mundo
+  Victor Pavel Cuentas Calle
+  modif: 2/5/23
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello world!\n" << endl;
+    return 0;
+}
